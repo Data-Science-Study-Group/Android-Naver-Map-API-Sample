35271d35f5f8ab1403f2f31a503bd1f730e1d4b8
tngusmiso <k1miso012@gmail.com>
2019-01-07 20:36:44 +0900

    [0107] Get Current Position (GPS)

---
+++ app/src/main/java/com/tngusmiso/mapsample/GeocodingMap.java
@@ -0,0 +1,13 @@
+package com.tngusmiso.mapsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeocodingMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geocoding_map);
+    }
+}
