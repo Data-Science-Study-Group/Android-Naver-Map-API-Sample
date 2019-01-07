560383fbf0b2e58e0b85bdeee5860f2cada493fb
tngusmiso <k1miso012@gmail.com>
2019-01-07 21:04:33 +0900

    [0107] Get Address and Location(LatLng)

--- app/src/main/java/com/tngusmiso/mapsample/MainActivity.java
+++ app/src/main/java/com/tngusmiso/mapsample/MainActivity.java
@@ -1,6 +1,12 @@
 package com.tngusmiso.mapsample;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,26 +19,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final int loactionpermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);//위치권한
+        if(loactionpermissionCheck == PackageManager.PERMISSION_DENIED){  // 권한거부 되있다면
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1); //권한허용창
+        }
+
         Button btBasic = (Button) findViewById(R.id.btBasic);
         Button btZoomPos = (Button) findViewById(R.id.btZoomPos);
+        Button btGeocoding = (Button) findViewById(R.id.btGeocoding);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent=new Intent();
                 switch (v.getId()) {
                     case R.id.btBasic:
-                        Intent intent = new Intent(MainActivity.this, BasicMap.class);
-                        startActivity(intent);
+                        intent = new Intent(MainActivity.this, BasicMap.class);
                         break;
                     case R.id.btZoomPos:
-                        Intent intent2 = new Intent(MainActivity.this, ZoomMap.class);
-                        startActivity(intent2);
+                        intent = new Intent(MainActivity.this, ZoomMap.class);
+                        break;
+                    case R.id.btGeocoding:
+                        intent = new Intent(MainActivity.this, GeocodingMap.class);
                         break;
                 }
+                startActivity(intent);
             }
         };
 
         btBasic.setOnClickListener(listener);
         btZoomPos.setOnClickListener(listener);
+        btGeocoding.setOnClickListener(listener);
     }
 }
